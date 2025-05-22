7ca91c501d61a6716647104bd9e0abe75ddf7989
sandrapabon <ctluna22@hotmail.com>
2025-05-22 11:54:02 -0500

    Descripción de los cambios realizados

--- GalaxyPay/src/navigation/AppNavigator.tsx
+++ GalaxyPay/src/navigation/AppNavigator.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import WelcomeScreen from '../screens/WelcomeScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import RegisterStep1Screen from '../screens/RegisterStep1Screen';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 
@@ -11,6 +14,8 @@ const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="RegisterStep1" component={RegisterStep1Screen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
